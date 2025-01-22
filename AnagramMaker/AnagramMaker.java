61d980a125295267181e385d99762eb8e625b1af
banan20081 <badran.banan@gmail.com>
2025-01-22 10:01:42 -0800

    Add files via upload

---
+++ AnagramMaker/AnagramMaker.java
@@ -0,0 +1,87 @@
+/**
+ *	AnagramMaker - <description goes here>
+ *
+ *	Requires the WordUtilities, SortMethods, Prompt, and FileUtils classes
+ *
+ *	@author	Banan Badran
+ *	@since	1/17/2025
+ */
+public class AnagramMaker {
+								
+	private final String FILE_NAME = "randomWords.txt";	// file containing all words
+	
+	private WordUtilities wu;	// the word utilities for building the word
+								// database, sorting the database,
+								// and finding all words that match
+								// a string of characters
+	
+	// variables for constraining the print output of AnagramMaker
+	private int numWords;		// the number of words in a phrase to print
+	private int maxPhrases;		// the maximum number of phrases to print
+	private int numPhrases;		// the number of phrases that have been printed
+		
+	/*	Initialize the database inside WordUtilities
+	 *	The database of words does NOT have to be sorted for AnagramMaker to work,
+	 *	but the output will appear in order if you DO sort.
+	 */
+	public AnagramMaker() {
+		wu = new WordUtilities();
+		wu.readWordsFromFile(FILE_NAME);
+		wu.sortWords();
+	}
+	
+	public static void main(String[] args) {
+		AnagramMaker am = new AnagramMaker();
+		am.run();
+	}
+	
+	/**	The top routine that prints the introduction and runs the anagram-maker.
+	 */
+	public void run() {
+		printIntroduction();
+		runAnagramMaker();
+		System.out.println("\nThanks for using AnagramMaker!\n");
+	}
+	
+	/**
+	 *	Print the introduction to AnagramMaker
+	 */
+	public void printIntroduction() {
+		System.out.println("\nWelcome to ANAGRAM MAKER");
+		System.out.println("\nProvide a word, name, or phrase and out comes their anagrams.");
+		System.out.println("You can choose the number of words in the anagram.");
+		System.out.println("You can choose the number of anagrams shown.");
+		System.out.println("\nLet's get started!");
+	}
+	
+	/**
+	 *	Prompt the user for a phrase of characters, then create anagrams from those
+	 *	characters.
+	 */
+	public void runAnagramMaker() {
+		String phrase;
+		System.out.println();
+		//do{
+			phrase = Prompt.getString("Word(s), name or phrase (q to quit)");
+		//}while(phrase!=null && phrase.length()<1);
+		if(phrase==null||phrase.equalsIgnoreCase("q")) return;
+		numWords = Prompt.getInt("Number of words in anagram");
+		maxPhrases = Prompt.getInt("Maximum number of anagrams to print");
+		System.out.println(phrase);
+		int searchNum=0;
+		
+		while(phrase.length()!=0){
+			//System.out.println(searchNum);
+			System.out.println(phrase.substring(0, phrase.indexOf(" ")));
+			phrase= phrase.substring(phrase.indexOf(" ")) ;
+			//recursiveFinder(phrase.substring(searchNum, phrase.indexOf(" ",searchNum)).trim());
+			
+		}
+	}
+//max sec =10
+// if number greater tan what it can do just print whatever it can then terminates
+	public void recursiveFinder(String phrase){
+		System.out.println("here "+phrase);
+	}
+	
+}
---
+++ AnagramMaker/FileUtils.java
@@ -0,0 +1,48 @@
+ import java.io.PrintWriter;
+ import java.io.File;
+ import java.io.FileNotFoundException;
+
+/*
+ *	File utilities for reading and writing
+ *
+ * @author	Banan
+ * @since August 23, 2024
+ */
+ 
+ 
+ public class FileUtils {
+	 
+	 /**
+	  * opens a file to readf using scanner class.
+	  * @param fileName		name of the file to open
+	  * @return 			the scanner object to the file
+	  */
+	 public static java.util.Scanner openToRead(String fileName){
+		 java.util.Scanner input = null;
+		 try {
+			 input = new java.util.Scanner(new java.io.File(fileName));
+		 }
+		 catch (java.io.FileNotFoundException e) {
+			 System.err.println("ERROR : Cannot open " + fileName + " For reading.");
+			 System.exit(1);
+		 }
+		 return input;
+	 }
+	 
+	 /**
+	  * opens a file to write using the PrintWriter class.
+	  * @param fileName		name of the file to open
+	  * @return 			PrintWriter object to the file
+	  */
+	 public static PrintWriter openToWrite(String fileName) {
+		 PrintWriter output = null;
+		 try{
+			 output = new PrintWriter(new File(fileName));
+		 }
+		 catch (FileNotFoundException e) {
+			 System.err.println("ERROR : Cannot open " + fileName + " For Writing.");
+			 System.exit(2);
+		 }
+		 return output;
+	 }
+ }
